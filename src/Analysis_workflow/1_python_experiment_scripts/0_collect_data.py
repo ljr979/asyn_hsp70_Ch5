78a47d6109703423c25b782cc2acf156b65fd3a1
ljr979 <ljr979@uowmail.edu.au>
2024-02-19 10:16:38 +1100

    docstrings

--- src/Analysis_workflow/1_python_experiment_scripts/0_collect_data.py
+++ src/Analysis_workflow/1_python_experiment_scripts/0_collect_data.py
@@ -1,3 +1,5 @@
+"""This script simply tranfers trajectories and colocalisation data from the experiment folder (where imageJ has output it) and sets up the folders and filenames so that they are identical for all experiments and the scripts that follow need less adjustment for each experiment.
+"""
 import os, re
 import pandas as pd
 import numpy as np
@@ -5,70 +7,52 @@ from loguru import logger
 import glob
 import shutil
 
-#this script simply tranfers trajectories and colocalisation data from the experiment folder (where imageJ has output it ) and sets up the folders and filenames so that they are identical for all experiments and the scripts that follow need less adjustment for each experiment.
+def move_files(input_folder, output_folder, folders):
+    """function to move and rename colocalisation and trajectory files from where they were output from image processing, into the repository with nomenclature that later scripts call on to carry treatment information etc
 
-#change these for experiment
-#top level experiment folder path
-input_folder = '/'
-output_folder = 'imagejresults/'
+    Args:
+        input_folder (str): top location of coloc and trajectory data
+        output_folder (str): location to output the files
+        folders (dict): dictionary with the folder the files exist in now, versus the folder nesting that you want them to be in in new location
+    """
+    for old_folder, (new_folder, filetype) in folders.items():
+        old_files = [filename for filename in os.listdir(f'{input_folder}{old_folder}') if not '_colocalized.csv' in filename]
+        if not os.path.exists(f'{output_folder}{new_folder}'):
+            os.makedirs(f'{output_folder}{new_folder}')
+        for x, filename in enumerate(old_files): 
+            shutil.copyfile(f'{input_folder}{old_folder}{filename}', f'{output_folder}{new_folder}{filetype}{x}.csv')
 
-if not os.path.exists(output_folder):
-    os.makedirs(output_folder)
+if __name__ == "__main__":
+        
+    #change these for experiment
+    #top level experiment folder path
+    input_folder = '/'
+    output_folder = 'imagejresults/'
 
-#this is what they will be called.
-#can change based on proteins in experiment
-hsp_colocalised = 'JB1_fibril_colocal_data'
-fibril_colocalised= 'HSPA8_fibril_colocal_data'
-hsps_coloc='JB1_HSPA8_colocal_data'
+    if not os.path.exists(output_folder):
+        os.makedirs(output_folder)
 
-# jb1_colocalised = 'DNAJB1_colocal_traj'
-#hspa8_colocal ='HSPA8_colocal_traj'
-hspa8_noncoloc ='HSPA8_non-colocal_traj'
-#change these for the experiment you're using
-folders = {
-#flow cell 1
-#treatment 1
-#coloc
-#'20230713_Experiment98_FC1-1_HSPA8-JB1-ATP/647/Colocalisation_analysis/Trajectories/AF647/coloc/':('FC1-1/Trajectories/Experiment98-A8-B1-flowout/coloc/', hspa8_colocal),
+    #this is what they will be called.
+    #can change based on proteins in experiment
+    hsp_colocalised = 'JB1_fibril_colocal_data'
+    fibril_colocalised = 'HSPA8_fibril_colocal_data'
+    hsps_coloc = 'JB1_HSPA8_colocal_data'
 
+    #jb1_colocalised = 'DNAJB1_colocal_traj'
+    #hspa8_colocal ='HSPA8_colocal_traj'
+    hspa8_noncoloc = 'HSPA8_non-colocal_traj'
+    #change these for the experiment you're using
+    folders = {
 
-
-
-#non-coloc
-'20230821_Experiment100b-FC1-fibrils-A8-B1-hsp110-5nM/647/non-coloc_trajectories/647/':('FC1-1/Trajectories/Experiment100-FC1-1_HSPA8_A8-B1-110-5nM/non-coloc/', hspa8_noncoloc),
-
-#coloc data
-'20230821_Experiment100b-FC1-fibrils-A8-B1-hsp110-5nM/647/Colocalisation_analysis/':('FC1-1/Colocalisation/Experiment100-FC1-1_HSPA8_A8-B1-110-5nM/fibrils-647/', fibril_colocalised),
-
-
-# #non-coloc
-'20230821_Experiment100b-FC1-fibrils-A8-B1-hsp110-0.5uM/647/non-coloc_trajectories/647/':('FC1-2/Trajectories/Experiment100-FC1-2_HSPA8_A8-B1-110-0.5uM/non-coloc/', hspa8_noncoloc),
-
-#coloc data
-'20230821_Experiment100b-FC1-fibrils-A8-B1-hsp110-0.5uM/647/Colocalisation_analysis/':('FC1-2/Colocalisation/Experiment100-FC1-2_HSPA8_A8-B1-110-0.5uM/fibrils-647/', fibril_colocalised),
-
-
-
-
-#treatment2 
-#non-coloc
-    '20230821_Experiment100b-FC2-fibrils-A8-B1-SOD1-5nM/647/non-coloc_trajectories/647/': ('Trajectories/non-coloc/Experiment100-FC2-1_HSPA8_A8-B1-SOD1/non-coloc/', hspa8_noncoloc),
+    #non-coloc
+    'file_location_imagej_output/647/non-coloc_trajectories/647/':('treatment1/Trajectories/Experiment_number-treatment1_protein_treatment2-concentration/non-coloc/', hspa8_noncoloc),
 
     #coloc data
-    '20230821_Experiment100b-FC2-fibrils-A8-B1-SOD1-5nM/647/Colocalisation_analysis/': ('FC1-2/Colocalisation/Experiment100-FC2-1_HSPA8_A8-B1-SOD1/fibrils-647/', fibril_colocalised),
+    'file_location_imagej_output/647/Colocalisation_analysis/':('treatment1/Colocalisation/Experiment_number-treatment1_protein_treatment2-concentration/fibrils-647/', fibril_colocalised),
+
+        }
 
 
-
-
-    }
-
-
-
-for old_folder, (new_folder, filetype) in folders.items():
-    old_files = [filename for filename in os.listdir(f'{input_folder}{old_folder}') if not '_colocalized.csv' in filename]
-    if not os.path.exists(f'{output_folder}{new_folder}'):
-        os.makedirs(f'{output_folder}{new_folder}')
-    for x, filename in enumerate(old_files): 
-        shutil.copyfile(f'{input_folder}{old_folder}{filename}', f'{output_folder}{new_folder}{filetype}{x}.csv')
+    move_files(input_folder, output_folder, folders)
 
 
