899b4559c30db3ad13b6c88c57ed6e5eb9cd7043
ljr979 <ljr979@uowmail.edu.au>
2024-02-19 10:55:04 +1100

    fix import of py4bleaching

--- src/Analysis_workflow/1_python_experiment_scripts/1_hsp_stoichiometry.py
+++ src/Analysis_workflow/1_python_experiment_scripts/1_hsp_stoichiometry.py
@@ -1,4 +1,4 @@
-from src.py4bleaching.py4bleaching import analysis
+from utilities.py4bleaching.py4bleaching import analysis
 
 #This script runs the py4bleaching pipeline on all the trajectories1_hsp_stoichiometry
 input_folder = 'python_results/Trajectories/'
@@ -6,7 +6,6 @@ output_folder = 'python_results/py4bleaching/Trajectories/coloc/'
 
 #change this according to the model that you'd like to use (from the repo with all the models)
 model_name = 'Model_2'
-
 
 
 analysis.pipeline(input_folder, output_folder, probability_threshold=0.7, model_name=model_name, x_norm=True)
