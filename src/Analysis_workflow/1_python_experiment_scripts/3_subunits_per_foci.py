95b40fbb700b7b223eb8807411ee32117da46bee
ljr979 <ljr979@uowmail.edu.au>
2024-02-19 12:18:22 +1100

    gather and plot molecule counts

--- src/Analysis_workflow/1_python_experiment_scripts/3_subunits_per_foci.py
+++ src/Analysis_workflow/1_python_experiment_scripts/3_subunits_per_foci.py
@@ -1,20 +1,10 @@
-import os, re
+"""This script gathers, organizes and plots the data from py4bleaching 
+
+"""
+import os
 import pandas as pd
-import numpy as np
 import matplotlib.pyplot as plt
 import seaborn as sns
-
-#This script gathers, organizes and plots the data from py4bleaching 
-
-input_folder='python_results/py4bleaching/'
-output_folder='python_results/subunits_per_foci/'
-#find stoichiometry files output from py4bleaching
-stoich_files =[[f'{root}/{filename}' for filename in files if 'molecule_counts.csv' in filename] for root, dirs, files in os.walk(f'{input_folder}')]
-stoich_files=[item for sublist in stoich_files for item in sublist ]
-
-if not os.path.exists(output_folder):
-        os.makedirs(output_folder)
-
 
 def gather_coloc_stoich(stoich_files):
     """gathers all of the molecule counts files, makes sure the paths make sense. gets some metadata (variable2, which is concentration, so we can split between treatments later/ identify things more easily), and assigns this variable to a column. 
@@ -27,56 +17,79 @@ def gather_coloc_stoich(stoich_files):
     """
     molecule_counts=[]
     for filepath in stoich_files:
-        if 'noncoloc' not in filepath:
-            filepath=filepath.replace('\\', '/')
-            data= pd.read_csv(filepath)
-            variable2s = data.variable2
-            concentrations=[]
-            for item in variable2s:
-                concentration=item.split('-')[0]
-                concentrations.append(concentration)
-            data['concentration']=concentrations
-            molecule_counts.append(data)
+             
+        filepath=filepath.replace('\\', '/')
+        data= pd.read_csv(filepath)
+        if 'non-coloc' not in filepath:
+             data['colocalisation']='coloc'
+        if 'datatype' in data.columns.tolist():
+             data.drop([col for col in data.columns.tolist() if 'datatype' in col], axis=1, inplace=True)
+        variable2s = data.variable2
+        concentrations=[]
+        for item in variable2s:
+            concentration=item.split('-')[0]
+            concentrations.append(concentration)
+        data['concentration']=concentrations
+
+        molecule_counts.append(data)
     return molecule_counts
 
+def plot_violins( df, order_of_experiment, x, y, palette):
+    """plots the number of subunits per colocalised foci. As a violinplot.
 
-molecule_counts=gather_coloc_stoich(stoich_files)
-#concatinate into a dataframe
-molecule_counts=pd.concat(molecule_counts)
-#define order you want it plotted
+    Args:
+        output_folder (str): wher eto save
+        df (df): dataframe with concatinated molecule count
+        order_of_experiment (list): list of treatments in order for plotting
+        x (str): the variable (column name) to plot on the x axis
+        y (str): volumn variable to plot on the y axis
+        palette (str): colour palette for plotting
+        ylim (int): max on y axis
+    """
+    ax = sns.boxplot(x=x,
+    y=y, 
+    data=df,  
+    order=order_of_experiment,
+    palette=palette)
+    plt.xticks(rotation=45)
+    ax.set_ylabel('# of subunits')
+    ax.set_xlabel('Treatment')
+    #ax.set_ylim(ylim)
+    plt.tight_layout()
+    plt.title(f'# of HSPA8 molecules per foci')
+    plt.show()
 
-
-#dictionary to convert long names to their treatment for easy plotting
-concentration_dict={
+if __name__ == "__main__":
     
-    'Experiment100b-fc1-1': 'A8+JB1+110-5nM',
-    'Experiment100b-fc1-2': 'A8+JB1+110-0.5uM',
-    'Experiment100b-fc2-1': 'A8+JB1+SOD1'
+    input_folder='data/Analysis_workflow/2_example_python_output/1_py4bleaching/Trajectories/'
+    output_folder='data/Analysis_workflow/2_example_python_output/3_subunits_per_foci/'
 
-    }
-molecule_counts['concentration']=molecule_counts['Experiment_number'].map(concentration_dict)
+    #find stoichiometry files output from py4bleaching
+    stoich_files =[[f'{root}/{filename}' for filename in files if 'molecule_counts.csv' in filename] for root, dirs, files in os.walk(f'{input_folder}')]
+    stoich_files=[item for sublist in stoich_files for item in sublist]
+    stoich_files= [x for x in stoich_files if 'all_combined' not in x]
 
-#drop columns you don't want
-molecule_counts.drop([col for col in molecule_counts.columns.tolist() if ' ' in col], axis=1, inplace=True)
-molecule_counts.drop([col for col in molecule_counts.columns.tolist() if 'last_step_small_mol_count' in col], axis=1, inplace=True)
-molecule_counts.drop([col for col in molecule_counts.columns.tolist() if 'single_step_mol_count' in col], axis=1, inplace=True)
-molecule_counts.drop([col for col in molecule_counts.columns.tolist() if 'max_fluorescence' in col], axis=1, inplace=True)
+    if not os.path.exists(output_folder):
+            os.makedirs(output_folder)
 
-#define the order to plot
-order_of_experiment = ['A8+JB1+110-5nM', 'A8+JB1+110-0.5uM', 'A8+JB1+SOD1']
-#plot molecule size as number of subunits per molecule, in a boxplot
-ax = sns.boxplot(x="concentration",
- y="all_small_mol_count", 
- data=molecule_counts,  
- order=order_of_experiment,
-  #scale='width',
-   palette='YlOrBr')
-plt.xticks(rotation=45)
-ax.set_ylabel('# of subunits')
-ax.set_xlabel('Concentration (nM)')
-ax.set_ylim(0,30)
-plt.tight_layout()
-plt.title(f'# of HSPA8 molecules per foci')
-plt.savefig(f'{output_folder}number_subunits_per_foci.png')
-plt.show()
+    molecule_counts=gather_coloc_stoich(stoich_files)
+    #concatinate into a dataframe
+    molecule_counts=pd.concat(molecule_counts)
+    #account for all molecules that are < 1 due to variation in step size
+    molecule_counts['all_small_mol_count']=molecule_counts['all_small_mol_count']+1
+    #dictionary to convert long names to their treatment for easy plotting
+    concentration_dict={
+        'Experiment98-fc1-1': 'Enum-treat1',
+        'Experiment98-fc1-2': 'Enum-treat2'
+
+        }
+    molecule_counts['concentration']=molecule_counts['Experiment_number'].map(concentration_dict)
+    order_of_experiment = ['Enum-treat1', 'Enum-treat2']
+
+    #drop columns you don't want
+    drop_list=['last_step_small_mol_count','single_step_mol_count','max_fluorescence', 'Unnamed: 0', 'Unnamed: 0.1']
+    molecule_counts.drop([col for col in molecule_counts.columns.tolist() if col in drop_list], axis=1, inplace=True)
+
+    #plot molecule size as number of subunits per molecule, in a boxplot
+    plot_violins(molecule_counts, order_of_experiment, x='concentration', y="all_small_mol_count", palette="YlOrBr")
 
